57616464b00de2779cf2dc2f2fa5c578a354f398
uxokpro1234 <81376237+UxusNoob@users.noreply.github.com>
2021-12-17 17:29:46 +0200

    Add files via upload

--- IpGenerator.java
+++ IpGenerator.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("How many ip should be generated? ");
-    int count = scanner.nextInt();
-    int a = 0;
-    while (a<count){
-      a +=1;
-      int rand1 = (int)(Math.random() * 256);
-      int rand2 = (int)(Math.random() * 256);
-      int rand3 = (int)(Math.random() * 256);
-      int rand4 = (int)(Math.random() * 256);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many ip should be generated? ");
+        int count = scanner.nextInt();
+        int a = 0;
+        String dot = ".";
+        while (a<count){
+            a += 1;
+            int rand1 = (int)(Math.random() * 256);
+            int rand2 = (int)(Math.random() * 256);
+            int rand3 = (int)(Math.random() * 256);
+            int rand4 = (int)(Math.random() * 256);
 
-    System.out.println(rand1+"."+rand2+"."+rand3+"."+rand4);
+            System.out.println(rand1+dot+rand2+dot+rand3+dot+rand4);
+        }
     }
-  }
 }
